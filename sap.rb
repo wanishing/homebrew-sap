4a6418013e78567d0e4320be52c9e93d61a1509c
Wanishing <>
2021-10-05 12:52:16 +0300

    version 0.0.1

--- sap.rb
+++ sap.rb
@@ -2,7 +2,7 @@ class Sap < Formula
   desc "kubectl wrapper for spark applications"
   homepage ""
   url "https://github.com/wanishing/sap/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "6af343365297f480b17baf53d440368187c0125894ae53362e47a733367474dd"
+  sha256 "8de86a57f4ab4135a10516bd4b2daa841b5fa4f037de04d783f3f3bb66c5afd7"
   license "MIT"
   version "0.0.1"
 
