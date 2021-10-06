3b3bef6e82c0a8aa33a64139e8bb0199b7d161a2
wanishing <38142361+wanishing@users.noreply.github.com>
2021-10-06 14:00:15 +0300

    Version 0.0.2

--- sap.rb
+++ sap.rb
@@ -2,14 +2,14 @@ class Sap < Formula
   desc "kubectl wrapper for spark applications"
   homepage ""
   url "https://github.com/wanishing/sap/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "8de86a57f4ab4135a10516bd4b2daa841b5fa4f037de04d783f3f3bb66c5afd7"
+  sha256 "22088db6210828f4998455938d4b2e6671526b69d981c76b95bcfb05567aef28"
   license "MIT"
-  version "0.0.1"
+  version "0.0.2"
 
   depends_on "borkdude/brew/babashka" => :build
 
   def install
-    bin.install "sap"
+    bin.install "sap.clj" => "sap"
   end
 
   test do
