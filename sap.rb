870c27b10b7bbf1e641f352c9a3bf8907ab5cc49
wanishing <38142361+wanishing@users.noreply.github.com>
2021-11-08 12:50:12 +0200

    sap 0.0.3
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- sap.rb
+++ sap.rb
@@ -1,10 +1,10 @@
 class Sap < Formula
   desc "kubectl wrapper for spark applications"
   homepage ""
-  url "https://github.com/wanishing/sap/archive/refs/tags/v0.0.2.tar.gz"
-  sha256 "d0640e0f791dfd0eeb40acd053c624342002caee4e87c569223555c43e333274"
+  url "https://github.com/wanishing/sap/releases/download/v0.0.3/sap-0.0.3-macos.tar.gz"
+  sha256 "7b7e90d160cdd8baa54bf7fb30e5c1d288a512854a44757464008fb5a1304436"
   license "MIT"
-  version "0.0.2"
+  version "0.0.3"
   
   depends_on "borkdude/brew/babashka" => :build
   
