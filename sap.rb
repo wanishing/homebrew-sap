031049bad3ee0e254484b807dee2d2480cbbc792
wanishing <tal.vanish@gmail.com>
2022-08-08 17:51:52 +0300

    update

--- sap.rb
+++ sap.rb
@@ -1,15 +1,17 @@
 class Sap < Formula
   desc "kubectl wrapper for spark applications"
   homepage ""
-  url "https://github.com/wanishing/sap/archive/refs/tags/v0.0.2.tar.gz"
-  sha256 "d0640e0f791dfd0eeb40acd053c624342002caee4e87c569223555c43e333274"
   license "MIT"
-  version "0.0.2"
-  
-  depends_on "borkdude/brew/babashka" => :build
-  
+  version "0.0.5"
+
+  if OS.linux?
+    url "https://github.com/wanishing/sap/releases/download/v0.0.5/mal-0.0.5-ubuntu-22.04.zip"
+  else
+    url "https://github.com/wanishing/sap/releases/download/v0.0.5/mal-0.0.5-macos-11.zip"
+  end
+
   def install
-    bin.install "sap.clj" => "sap"
+    bin.install "sap"
   end
 
   test do
