728200f1edd2410993ef76361cc64167e2d5cf5d
wanishing <tal.vanish@gmail.com>
2022-10-03 15:19:00 +0300

    1.0.1

---
+++ Formula/sap@1.0.1.rb
@@ -0,0 +1,22 @@
+class SapAT101 < Formula
+  desc "kubectl wrapper for spark applications"
+  homepage ""
+  license "MIT"
+  version "1.0.1"
+
+  if OS.linux?
+    url "https://github.com/wanishing/sap/releases/download/v1.0.1/sap-1.0.1-ubuntu-22.04.zip"
+    sha256 "bca5df2acd487aecd69188989499ce58b267479c23910ae56126b0b69fe37753"
+  else
+    url "https://github.com/wanishing/sap/releases/download/v1.0.1/sap-1.0.1-macos-11.zip"
+    sha256 "ff9acd24d471576fc6bf9ffb32089fa58f5b0003615b02c14a28d6157819aa90"
+  end
+
+  def install
+    bin.install "sap"
+  end
+
+  test do
+    %x[sap --help]
+  end
+end
